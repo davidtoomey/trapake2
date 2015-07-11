518f899e826b73ab603e4d2b6e6219210a8394e7
David Toomey <d.toomey@temple.edu>
2015-07-10 22:17:02 -0400

    first commit

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,10 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+Rails.application.config.assets.precompile += %w( normalize.css )
+Rails.application.config.assets.precompile += %w( officialtrapapp.webflow.css )
+Rails.application.config.assets.precompile += %w( webflow.css )
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
